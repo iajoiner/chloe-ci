af33f1a5df35ce216654698b852ecc40423c92fe
Ian Alexander Joiner <14581281+iajoiner@users.noreply.github.com>
2023-03-07 00:11:05 -0500

    feat: add tests & update .gitignore

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, Chloe CI!");
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn cats_are_cats() {
+        let result = "cats";
+        assert_eq!(result, "cats");
+    }
+
+    #[test]
+    fn cats_are_not_dogs() {
+        let result = "cats";
+        assert_ne!(result, "dogs");
+    }
+}
